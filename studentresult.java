b0f6919719a7ec08c6d0909ef0fdb593d17c6788
pannalalbengani05 <56900308+pannalalbengani05@users.noreply.github.com>
2019-10-23 06:07:51 +0530

    Create studentresult.java

---
+++ studentresult.java
@@ -0,0 +1,22 @@
+class StudentResult{
+
+    String name;
+    String rollno;
+    double marks;
+
+    StudentResult()
+    {
+    }
+
+    StudentResult(String name,String rollno,double marks)
+    {
+        this.name = name;
+        this.rollno = rollno;
+        this.marks = marks;
+    }
+    
+
+    
+
+
+}
